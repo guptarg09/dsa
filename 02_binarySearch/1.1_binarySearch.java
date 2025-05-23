ad08719e8f18f5404b86dc09ba842727f0620c54
Ritesh Gupta <guptarg3a@gmail.com>
2025-05-23 13:47:39 +0000

    done

---
+++ 02_binarySearch/1.1_binarySearch.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/binary-search/
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int low = 0; 
+        int high = nums.length-1;
+        
+        while(low <= high){
+
+            // int mid = high + (low - high) / 2;
+            int mid = (low + high) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
