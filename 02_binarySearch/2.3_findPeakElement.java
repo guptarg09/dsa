49dee85a606a8d44d0a98aec3cae7d23ed4fe103
Ritesh Gupta <guptarg3a@gmail.com>
2025-06-06 17:15:56 +0000

    done the problem

---
+++ 02_binarySearch/2.3_findPeakElement.java
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/find-peak-element
+
+// brootforce solution
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if((i == 0 || nums[i] > nums[i-1]) && i == n-1 || nums[i] > nums[i+1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// optimal solution
+class Solution {
+    public int findPeakElement(int[] nums) {
+        
+        int n = nums.length;
+
+        if(n == 1){
+            return 0;
+        }
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[n-1] > nums[n-2]){
+            return n-1;
+        }
+
+        int low = 1;
+        int high = n-2;
+        while(low <= high){
+
+            int mid = low + (high - low) / 2;
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid] > nums[mid-1]){
+                low = mid + 1;
+            }
+            else if(nums[mid] > nums[mid+1]){
+                high = mid - 1;
+            }
+            // [2,1,3] for this case
+            else{
+                low = mid + 1;
+                // high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
