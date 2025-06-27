ad990d7f0dc33a8796ad030d8eb8f16a48b4ef7f
Ritesh Gupta <guptarg3a@gmail.com>
2025-06-27 17:51:04 +0000

    done the problem

---
+++ 02_binarySearch/3.5_serchInRowColumnWiseSortedMatrix.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/search-a-2d-matrix-ii
+
+// optimal solution
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0;
+        int col = m-1;
+
+        while(row < n && col >= 0){
+
+            if(matrix[row][col] == target){
+                return true; 
+            }
+            else if(matrix[row][col] < target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+        return false;
+    }
+}
