d7470258939cbb1e4ba9e2740967d940e41eea17
Ritesh Gupta <guptarg3a@gmail.com>
2025-11-18 12:36:53 +0000

    done fibonacci problem

---
+++ recursion/Fibo.java
@@ -0,0 +1,15 @@
+package recursion;
+
+// fibonacci series
+
+public class Fibo {
+    public static void main(String[] args){
+        System.out.println(fiboNumber(6));
+    }
+    static int fiboNumber(int num){
+        if(num < 2){
+            return num;
+        }
+        return fiboNumber(num - 1) + fiboNumber(num - 2);
+    }
+}
