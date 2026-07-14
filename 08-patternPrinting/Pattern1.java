eb73fc3adba93854143156a6b5d2f464cabb6f23
Ritesh Gupta <guptarg3a@gmail.com>
2026-07-14 15:04:28 +0000

    done pattern1

---
+++ 08-patternPrinting/Pattern1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        pattern(n);
+    }
+    public static void pattern(int n) {
+        for(int row = 1; row <= n; row++) {
+            for(int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
