663bd05d0b5bf632a87bfa48cf4236b0355325f6
Ritesh Gupta <guptarg3a@gmail.com>
2026-07-14 15:07:03 +0000

    done pattern 2

---
+++ 08-patternPrinting/Pattern2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        pattern(n);
+    }
+    public static void pattern(int n) {
+        for(int row = 1; row <= n; row++) {
+            for(int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
