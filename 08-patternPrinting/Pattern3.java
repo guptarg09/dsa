07689e51fbc017ffdc78f9338cb585d119cfc3ef
Ritesh Gupta <guptarg3a@gmail.com>
2026-07-15 10:00:36 +0000

    refactor class name

--- 08-patternPrinting/Pattern3.java
+++ 08-patternPrinting/Pattern3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pattern2 {
+public class Pattern3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number");
