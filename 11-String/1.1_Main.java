8d3ba06526546d372176f325416c7e53bd46086d
Ritesh Gupta <guptarg3a@gmail.com>
2026-07-23 10:46:49 +0000

    add initial implementation of Main class with string manipulation

---
+++ 11-String/1.1_Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Ritesh Gupta";
+    //   System.out.println(name);
+
+        String a = "Ritesh";
+        System.out.println(a);
+        a = "Gupta";
+        System.out.println(a);
+    }
+}
